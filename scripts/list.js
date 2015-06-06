668c839b0ce5a54ea6fea0eadd2019dc552fae3c
lbarberiscanoni <hllbck7@gmail.com>
2015-06-05 17:31:47 -0700

    fixed the list problem so it shows the appropriate bills for each school

--- scripts/list.js
+++ scripts/list.js
@@ -4,14 +4,24 @@ $(document).ready(function() {
     fireData.orderByValue().on("value", function(snapshot) {
         snapshot.forEach(function(data) {
             school = data.key()
-            $("body").append("<h3>" + school + "</h3>");
-
             billList = new Firebase(fireData + "/" + school);
+
             billList.orderByValue().on("value", function(snapshot) {
                 snapshot.forEach(function(data) {
                     author = data.key();
                     billTitle = data.val().billTitle;
-                    $("body").append("<p>" + author + ": " + billTitle + "</p>");
+
+                    //Setting up the container
+                    $("div.container").append("<h3>" + school + "</h3>");
+                    $("div.container").append("<div>" + "</div>");
+                    $("div.container div:last-of-type").addClass(school);
+
+                    //Adding the bill info
+                    $("div." + school).append("<p>" + author + ": " + billTitle + "</p>");
+                    $("div." + school).append("<button>" + "See Bill" + "</button>");
+                    $("button").attr("type", "submit");
+                    $("button").addClass("btn btn-default");
+                    $("button").attr("id", "seeBill");
                 });
             });
         });
