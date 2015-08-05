d1884d6a3618319f1ee17931dd8b646d4df40c9b
lbarberiscanoni <hllbck7@gmail.com>
2015-08-05 15:56:46 -0700

    finalized the amendment process for the bill so now clerks can alter the bill in real time

--- scripts/seeBill.js
+++ scripts/seeBill.js
@@ -6,6 +6,7 @@ var thisBillTitle = $("h1").text();
 billList.on("child_added", function(snapshot) {
     var bill = snapshot.val();
     if (bill.billTitle == thisBillTitle) {
+        var thisBillID = bill.id;
         $(".container").append("<h3 class='text-center'>" + "BE IT HEREBY ENACTED BY THE YMCA MODEL LEGISLATURE OF SOUTH CAROLINA" + "</h3>");
         sectionsOfThisBill = ["Section1", bill.section1,"Section 2", bill.section2, "Section 3", bill.section3, "Section 4", bill.section4, "Section 5", "This bill shall be enacted on " + bill.section5];
         for (var i = 0; i < sectionsOfThisBill.length / 2; i++) {
@@ -19,17 +20,56 @@ billList.on("child_added", function(snapshot) {
             alert("start editing");
             $(".amendable").attr("contenteditable", "true");
             $(".amendable").click(function() {
-                var thisSection = $(this).prev().text().split(" ").join("").toLowerCase();
-                var dataToChange = bill.thisSection;
-                var oldVersion = $(this).text();
+                var thisInfo = $(this);
+                var sectionToAmend = $(this).prev().text().split(" ").join("").toLowerCase();
+                if (sectionToAmend == "section1") {
+                    var currentText = bill.section1;
+                } else if (sectionToAmend == "section2") {
+                    var currentText = bill.section2;
+                } else if (sectionToAmend == "section3") {
+                    var currentText = bill.section3;
+                } else if (sectionToAmend == "section4") {
+                    var currentText = bill.section4;
+                } else if (sectionToAmend == "section5") {
+                    var currentText = bill.section5;
+                } else {
+                    console.log("error: section not amendable");
+                };
+                
+                //submit the amendment
                 $("<button id='submitChanges'>" + "Submit Amendment" + "</button>").insertAfter("#amend");
                 $("#submitChanges").addClass("btn btn-default");
                 $("#submitChanges").click(function() {
-                    var newVersion = $(".amendable").text();
-                    alert("old version " + oldVersion);
-                    alert("new version " + newVersion);
+                    var amendment = thisInfo.text();
+                    alert("amending " + sectionToAmend + " from [" + currentText + "] to [" + amendment + "]");
+ 
+                    //let's push the amendment to the DB
+                    if (sectionToAmend == "section1") {
+                        billList.child(thisBillID).update({
+                            "section1": amendment,
+                        });
+                    } else if (sectionToAmend == "section2") {
+                        billList.child(thisBillID).update({
+                            "section2": amendment,
+                        });
+                    } else if (sectionToAmend == "section3") {
+                        billList.child(thisBillID).update({
+                            "section3": amendment,
+                        });
+                    } else if (sectionToAmend == "section4") {
+                        billList.child(thisBillID).update({
+                            "section4": amendment,
+                        });
+                    } else if (sectionToAmend == "section5") {
+                        billList.child(thisBillID).update({
+                            "section5": amendment,
+                        });
+                    } else {
+                        console.log("error: amendment couldn't go through because it was not done in any of the avaiblable sections not amendable");
+                    };
+                    
+                    //let's go back to normal
                     $(".amendable").attr("contenteditable", "false");
-                    window.location.reload();
                     alert("amendment made");
                     window.close();
                 });
