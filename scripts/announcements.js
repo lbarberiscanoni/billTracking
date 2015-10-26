c5900a5ee27ae2b2aa5d6a89b0a87698642c1f5d
lbarberiscanoni <hllbck7@gmail.com>
2015-10-25 21:05:58 -0700

    1) added the new pages to build a web interface for the data that shows up on the mobile app 2) finished the firs tdraft of the annoucnments and staffQuestions

---
+++ scripts/announcements.js
@@ -0,0 +1,29 @@
+var listOfAnnouncements = new Firebase("https://yig-bill-tracker.firebaseio.com/announcements");
+
+$(document).ready(function() {
+    //option to make new announcements
+    $("#submit").click(function() {
+        var announcementTitle = $("#announcementTitle").val();
+        var announcementText = $("#announcementText").val();
+        var timeStamp = new Date().getTime();
+        var timeStamp = parseInt(timeStamp / 1000);
+        listOfAnnouncements.push({
+            title: announcementTitle,
+            content: announcementText,
+            timestamp: timeStamp,
+        });
+        alert("success!");
+        window.location.reload();
+    });
+
+    //showing all annoucnments in order of recency
+    listOfAnnouncements.on("child_added", function(snapshot) {
+        var announcement = snapshot.val();
+        var timeStamp = announcement.timestamp;
+        var timeStamp = parseInt(timeStamp * 1000);
+        var timeStamp = new Date(timeStamp);
+        var timeStamp = timeStamp.toString().split("GMT")[0];
+        var timeStamp = timeStamp.split("2015")[0] + " @ " + timeStamp.split("2015")[1];
+        $("<div class='row'><div class='col-md-4'></div><div class='col-md-4'><h3>" + announcement.title + "</h3><h4>" + timeStamp + "</h4></h3><p>" + announcement.content + "</p></div><div class='col-md-4'></div></div>").insertAfter("#pastAnnouncements h2");
+    });
+});
---
+++ scripts/staffQuestions.js
@@ -0,0 +1,30 @@
+var listOfQuestions = new Firebase("https://yig-bill-tracker.firebaseio.com/staff-questions")
+
+$(document).ready(function() {
+    listOfQuestions.on("child_added", function(snapshot) {
+        var question = snapshot.val();
+        if (question.status == "not answered") {
+            $("#questions").prepend("<div class='row'><div class='row'><div class='row'><div class='row'><div class='row'><div class='row'><div class='row'><div class='row'><div class='row'><div class='col-md-4'></div><div class='col-md-4'><table class='table table-bordered'><tr><td>From: " + question.source + "</td><td>At: " + question.sentTimestamp + "</td><td id='questionID'>ID: " + snapshot.key() + "</td></tr><tr><td colspan='3' class='text-center' id='textOfTheQuestion'>" + question.question + "</td></tr><td colspan='3'><div class='col-md-4'></div><div class='col-md-4'><button class='btn btn-primary' id='answerQuestion'>Answer Question</button></td></div><div class='col-md-4'></div></table></div><div class='col-md-4'></div></div>");
+            $("#answerQuestion").click(function() {
+                answeringState = $(this).text();
+                if (answeringState == "Answer Question") {
+                    $(this).text("Submit Answer");
+                    var bro = $("#answerQuestion").parent().parent().parent();
+                    $("<tr><td colspan='3'><textarea rows='4' cols='65' id='answerToTheQuestion'></textarea></td></tr>").insertBefore(bro);
+                } else if (answeringState == "Submit Answer") {
+                    var answerToTheQuestion = $("#answerToTheQuestion").val();
+                    var questionID = snapshot.key();
+                    var timeStamp = new Date().getTime(); 
+                    listOfQuestions.child(questionID).update({
+                        status: "answered",
+                        answer: answerToTheQuestion,
+                        answeredTimestamp: timeStamp,
+                    });
+                    window.location.reload();
+                } else {
+                    alert("error");
+                };
+            });
+        };
+    });
+});
