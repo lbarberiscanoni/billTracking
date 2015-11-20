87c56d5c8ad676c4e867f76179d1ad11aa67fe48
lbarberiscanoni <hllbck7@gmail.com>
2015-11-20 11:59:36 -0500

    finisehd the new bill search engine

--- scripts/alterBill.js
+++ scripts/alterBill.js
@@ -8,7 +8,29 @@ $(document).ready(function() {
             $(".amendable").attr("contenteditable", "true");
         } else {
             var lol = $(".amendable");
-            console.log(lol);
+            var infoList = [];
+            for (i = 0; i < lol.length; i++) {
+                console.log(lol[i].innerHTML);
+                infoList.push(lol[i].innerHTML);
+            };
+            console.log(infoList);
+            var updateBillInfo = function() {
+                listOfBills.child(infoList[0]).update({
+                    school: infoList[1],
+                    author1: infoList[2],
+                    author2: infoList[3],
+                    authorLocation: infoList[4],
+                    billStatus: infoList[5],
+                    billLocation: infoList[6],
+                    division: infoList[7],
+                    rocketDocketStatus: infoList[8],
+                    sponsor: infoList[9],
+                    governorEvaluation: infoList[10],
+                    billTitle: infoList[11],
+                    billText: infoList[12],
+                });
+            };
+            updateBillInfo();
         };
     });
 });
